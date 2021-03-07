08f0abaeb694a48858aa54cfa10c624e832929f4
upsicleclown <55859815+upsicleclown@users.noreply.github.com>
2021-03-07 02:37:14 -0500

    coin dart

--- lib/mathimino.dart
+++ lib/mathimino.dart
@@ -5,6 +5,7 @@ import 'package:flame/components/component.dart';
 import 'package:flame/sprite.dart';
 import 'package:flame/util.dart';
 import 'package:mathimino/backg.dart';
+import 'package:mathimino/coin.dart';
 
 class Mathimino extends BaseGame {
   SpriteComponent _eqBlock;
@@ -14,28 +15,31 @@ class Mathimino extends BaseGame {
   final int numOfHorizTiles = 7; //total number of horizontal tiles
   final int rowNumTiles = 5; // number of tiles that a row takes up
   BackgroundLayer bLayer;
+  Coin coin;
 
   Mathimino() {
     _eqBlock = SpriteComponent.square(64, 'redBrick.png');
     this.add(_eqBlock);
-    row1 = SpriteComponent.rectangle(origRowWidth.toDouble(), origRowHeight.toDouble(), 'PurpleBlockRow.png');
+    row1 = SpriteComponent.rectangle(origRowWidth.toDouble(),
+        origRowHeight.toDouble(), 'PurpleBlockRow.png');
     row1.x = 30;
     row1.y = 500;
     this.add(row1);
     //if (size != null){
-    bLayer = BackgroundLayer('backGround.png', 'leftTower.png', 'rightTower.png');
-    
+    bLayer =
+        BackgroundLayer('backGround.png', 'leftTower.png', 'rightTower.png');
+    coin = Coin();
+    this.add(coin.component);
   }
-  void resize(Size size){
+  void resize(Size size) {
     super.resize(size);
-    row1.width = size.width*rowNumTiles/numOfHorizTiles;
-    row1.height = origRowHeight/origRowWidth*row1.width;
-    
+    row1.width = size.width * rowNumTiles / numOfHorizTiles;
+    row1.height = origRowHeight / origRowWidth * row1.width;
   }
-  void update (double t){
-    
-  }
+
+  void update(double t) {}
   void render(Canvas canvas) {
     bLayer.drawBackground(canvas, size);
+    coin.draw(canvas);
   }
 }
